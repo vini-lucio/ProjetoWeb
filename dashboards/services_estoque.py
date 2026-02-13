367397bd8a6b3935e40a31e3336c8d71cf700419
Vinicius Lucio <vini_lucio_oliveira@hotmail.com>
2026-02-13 15:53:54 -0300

    Correção endereços vazio no dashboard de estoque

--- dashboards/services_estoque.py
+++ dashboards/services_estoque.py
@@ -30,33 +30,40 @@ class DashBoardEstoque():
         quantidade_enderecos_ocupados = quantidade_enderecos_ocupados.rename(
             columns={'quantidade': 'quantidade_ocupada', 'tipo_produto__descricao': 'tipo_produto'})
 
-        enderecos = pd.merge(quantidade_enderecos_validos, quantidade_enderecos_ocupados,
-                             'left', ['tipo', 'tipo_produto']).fillna(0)
-        enderecos['quantidade_disponivel'] = enderecos['quantidade_total'] - enderecos['quantidade_ocupada']
+        if not quantidade_enderecos_validos.empty and not quantidade_enderecos_ocupados.empty:
+            enderecos = pd.merge(quantidade_enderecos_validos, quantidade_enderecos_ocupados,
+                                 'left', ['tipo', 'tipo_produto']).fillna(0)
+        else:
+            enderecos = quantidade_enderecos_validos.copy()
+            enderecos['quantidade_ocupada'] = 0
 
-        # Ocupação endereços de produto acabado
+        if not quantidade_enderecos_validos.empty:
+            enderecos['quantidade_disponivel'] = enderecos['quantidade_total'] - enderecos['quantidade_ocupada']
 
-        self.enderecos_produto_acabado = enderecos[enderecos['tipo_produto'] == 'PRODUTO ACABADO'].copy()
-        self.enderecos_totais_produto_acabado = pd.DataFrame(
-            [self.enderecos_produto_acabado[['quantidade_total', 'quantidade_ocupada']].sum()])
+            # Ocupação endereços de produto acabado
 
-        self.enderecos_produto_acabado['ocupacao'] = self.enderecos_produto_acabado['quantidade_ocupada'] / \
-            self.enderecos_produto_acabado['quantidade_total'] * 100
-        self.enderecos_totais_produto_acabado['ocupacao'] = self.enderecos_totais_produto_acabado['quantidade_ocupada'] / \
-            self.enderecos_totais_produto_acabado['quantidade_total'] * 100
+            self.enderecos_produto_acabado = enderecos[enderecos['tipo_produto'] == 'PRODUTO ACABADO'].copy()
+            self.enderecos_totais_produto_acabado = pd.DataFrame(
+                [self.enderecos_produto_acabado[['quantidade_total', 'quantidade_ocupada']].sum()])
 
-        self.enderecos_produto_acabado = self.enderecos_produto_acabado.to_dict(orient='records')
-        self.enderecos_totais_produto_acabado = self.enderecos_totais_produto_acabado.to_dict(orient='records')
+            self.enderecos_produto_acabado['ocupacao'] = self.enderecos_produto_acabado['quantidade_ocupada'] / \
+                self.enderecos_produto_acabado['quantidade_total'] * 100
+            self.enderecos_totais_produto_acabado['ocupacao'] = self.enderecos_totais_produto_acabado['quantidade_ocupada'] / \
+                self.enderecos_totais_produto_acabado['quantidade_total'] * 100
 
-        # Ocupação endereços de materia prima
+            self.enderecos_produto_acabado = self.enderecos_produto_acabado.to_dict(orient='records')
+            self.enderecos_totais_produto_acabado = self.enderecos_totais_produto_acabado.to_dict(orient='records')
 
-        self.enderecos_mp = enderecos[enderecos['tipo_produto'] != 'PRODUTO ACABADO'].copy()
-        self.enderecos_totais_mp = pd.DataFrame([self.enderecos_mp[['quantidade_total', 'quantidade_ocupada']].sum()])
+            # Ocupação endereços de materia prima
 
-        self.enderecos_mp['ocupacao'] = self.enderecos_mp['quantidade_ocupada'] / \
-            self.enderecos_mp['quantidade_total'] * 100
-        self.enderecos_totais_mp['ocupacao'] = self.enderecos_totais_mp['quantidade_ocupada'] / \
-            self.enderecos_totais_mp['quantidade_total'] * 100
+            self.enderecos_mp = enderecos[enderecos['tipo_produto'] != 'PRODUTO ACABADO'].copy()
+            self.enderecos_totais_mp = pd.DataFrame(
+                [self.enderecos_mp[['quantidade_total', 'quantidade_ocupada']].sum()])
 
-        self.enderecos_mp = self.enderecos_mp.to_dict(orient='records')
-        self.enderecos_totais_mp = self.enderecos_totais_mp.to_dict(orient='records')
+            self.enderecos_mp['ocupacao'] = self.enderecos_mp['quantidade_ocupada'] / \
+                self.enderecos_mp['quantidade_total'] * 100
+            self.enderecos_totais_mp['ocupacao'] = self.enderecos_totais_mp['quantidade_ocupada'] / \
+                self.enderecos_totais_mp['quantidade_total'] * 100
+
+            self.enderecos_mp = self.enderecos_mp.to_dict(orient='records')
+            self.enderecos_totais_mp = self.enderecos_totais_mp.to_dict(orient='records')
